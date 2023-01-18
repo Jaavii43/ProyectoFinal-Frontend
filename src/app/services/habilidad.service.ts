f3cb1e0699fcdaf94a530622b696cef0c8b54cb0
Jaavii43 <jatoro_2001@yahoo.com.ar>
2023-01-18 11:19:59 -0300

    Update habilidad.service.ts

--- src/app/services/habilidad.service.ts
+++ src/app/services/habilidad.service.ts
@@ -7,15 +7,15 @@ import { Habilidade } from '../model/habilidade';
   providedIn: 'root'
 })
 export class HabilidadService {
-  url:string="http://localhost:8080/habilidad"; 
+  URL : 'http://localhost:8080/habilidad/'; 
   
   constructor(private httpClient:HttpClient) { }
 
   public list():Observable<Habilidade[]>{
-    return this.httpClient.get<Habilidade[]>(this.url + `ver/habilidades`);
+    return this.httpClient.get<Habilidade[]>(this.URL + '/ver');
   }
 
   public detail(id:number) : Observable <Habilidade> {
-    return this.httpClient.get<Habilidade>(this.url + `detail/habilidades/$(id)`);
+    return this.httpClient.get<Habilidade>(this.URL + 'traer/$(id)');
   }
 }
