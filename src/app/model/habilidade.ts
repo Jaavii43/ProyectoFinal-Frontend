f527c9a5f0f39afcd9a53d63581d11a5ac7ae76d
Jaavii43 <jatoro_2001@yahoo.com.ar>
2023-02-08 17:02:22 -0300

    ssss

--- src/app/model/habilidade.ts
+++ src/app/model/habilidade.ts
@@ -1,12 +1,13 @@
 export class Habilidade {
 
-    id?:number;
+    id:number;
     area:string;
     porcentaje:number;  
 
-    constructor(area:string, porcentaje:number){
+    constructor(area:string, porcentaje:number, id:number){
 
         this.area = area;
         this.porcentaje = porcentaje;
+        this.id=id;
     }
 }
